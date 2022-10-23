e02779ffdbec2d2980e2e782dd618a70711c4ed7
LySeriuos <drungilas.p@gmail.com>
2022-10-23 17:26:39 +0200

    Add project files.

---
+++ The_Halloween_Hangman/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace The_Halloween_Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomWord = new Random(); // activating random function
+            var words = new List<string>() { "money", "danger", "aloof", "scheme", "observer", "abolish", "goalkeeper",
+                "conviction", "allowance", "arrogant", "separation", "environment",
+                "protection", "deteriorate", "economist", "continental", "mushroom",
+                "repetition", "unlawful", "arrogant", "jazz" };
+
+            
+        }
+    }
+}
