f06f7831c834cc959aa875ec5410243a6a41a63d
LySeriuos <drungilas.p@gmail.com>
2022-10-23 19:20:11 +0200

    created the list and getting random word from the list. Printing outrandom word letters(char)

--- The_Halloween_Hangman/Program.cs
+++ The_Halloween_Hangman/Program.cs
@@ -7,13 +7,20 @@ namespace The_Halloween_Hangman
     {
         static void Main(string[] args)
         {
-            Random randomWord = new Random(); // activating random function
+            Random random = new Random(); // activating random function
             var words = new List<string>() { "money", "danger", "aloof", "scheme", "observer", "abolish", "goalkeeper",
                 "conviction", "allowance", "arrogant", "separation", "environment",
                 "protection", "deteriorate", "economist", "continental", "mushroom",
                 "repetition", "unlawful", "arrogant", "jazz" };
 
-            
+            int wordsIndex = random.Next(words.Count); // picking random index in the list
+            string randomWord = words[wordsIndex]; // getting the value of the index
+            Console.WriteLine(randomWord); // printing random word in the Console
+
+            foreach (char caracter in randomWord) // spelling letters for the random word
+            {
+                Console.WriteLine(caracter);
+            }
         }
     }
 }
