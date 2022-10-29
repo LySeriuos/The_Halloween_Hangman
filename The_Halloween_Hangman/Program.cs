d8d0ac0d5d81d00eda0dfeac0823c39aca040fe3
LySeriuos <drungilas.p@gmail.com>
2022-10-29 20:48:40 +0200

    finding the matching letters indexes

--- The_Halloween_Hangman/Program.cs
+++ The_Halloween_Hangman/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.ComponentModel;
-using System.Data.SqlTypes;
-using System.Text.RegularExpressions;
-using System.Xml.Linq;
-using static System.Net.Mime.MediaTypeNames;
-
-namespace The_Halloween_Hangman
+﻿namespace The_Halloween_Hangman
 {
     internal class Program
     {
@@ -16,38 +9,96 @@ namespace The_Halloween_Hangman
                 "conviction", "allowance", "arrogant", "separation", "environment",
                 "protection", "deteriorate", "economist", "continental", "mushroom",
                 "repetition", "unlawful", "arrogant", "jazz" };
-            List<char[]> letters;
             int wordsIndex = random.Next(words.Count); // picking random index in the list
             string randomWord = words[wordsIndex]; // getting the value of the index
             Console.WriteLine(randomWord); // printing random word in the Console
-            char[] charArr = randomWord.ToCharArray(); // dividing word in chars and adding to array
-            foreach (char ch in charArr)
+            //char[] charArr = randomWord.ToCharArray(); // dividing word in chars and adding to array          
+
+            //var good = charArr.ToString();
+           // Console.Write(good);
+
+            // char charTry = char.Parse(userInput);
+            // char[] userChars = userInput.ToCharArray();
+            // char[] charArrInput = userInput.ToCharArray();
+           //bool validUserInput = randomWord.Contains(userInput); // validating user's input
+          // bool validUserInput2 = randomWord.Contains(userInput);
+                      
+            int found;
+            int totFinds = 0;
+            int i;
+            // 1. make and for loop which is exact length as the random word and print underscores
+            // 2. this part of the code is getting the indexes of matching letter with userInput 
+            // 3. later I need to change empty indexes to the letter it was matching
+            do
             {
-                Console.WriteLine(ch);
-            }
+                Console.WriteLine("Guess the letter!"); // Asking for user's input
+                string userInput = Console.ReadLine(); // reading user's input
+                bool validUserInput = randomWord.Contains(userInput);
+
+            if (userInput != "") // check if user input is not empty 
+                {
+                    for (i = 0; i < randomWord.Length; i++) // check thru the chosen word 
+                    {
+                        found = randomWord.IndexOf(userInput, i); //getting the indexes of the letters                          
+
+                        if (found >= 0) // if index is gigher than 0 we can count how many times the same letter was found
+                        {
+                            totFinds++; //counting finds
+                            i = found; // getting index numbers
+                            Console.WriteLine(i);
+                        }
+                        else
+                        {                            
+                            break;                            
+                        }                        
+                    }
+                    
+                }
+                else
+                {                    
+                return;
+                }
+
+                Console.WriteLine("{0}The search parameter '{1}' was found {2} times.",
+                        Environment.NewLine, userInput, totFinds);
+
+                totFinds = 0;
+                if (validUserInput != true) // if the users input doesn't match any letter it gives this error
+                {
+                    Console.WriteLine("Your letter doesn't match with any in the word");
+                }
+            } while (true);
+                
 
 
 
 
 
-           //for (int i = 0; i < randomWord.Count(); i++)
-           //{
-           //    Console.Write("the letters"[i]);
-           //    //Regex.Replace(randomWord, "([A-Z])", "_$1");
-           //    //Console.WriteLine(Regex.Replace(randomWord[i], "([A-Z])", "_$1"));
-           //}
-            Console.WriteLine("Guess the letter!");
-            
-            string userInput = Console.ReadLine();
-            bool validUserInput = randomWord.Contains(userInput);
-            
-            
-           // Console.WriteLine("The string '{0}' contains '{1}'? -> '{0}'", randomWord, userInput, validUserInput, words);
-            Console.WriteLine($"The string '{randomWord}' contains '{userInput}'? -> '{validUserInput}'");
-            if (validUserInput)
-            {
-                Console.WriteLine("Working");
-            }
+
+
+
+
+
+
+
+
+
+
+
+
+            //for (int i = 0; i < randomWord.Count(); i++)
+            //{
+            //    Console.Write("the letters"[i]);
+            //    //Regex.Replace(randomWord, "([A-Z])", "_$1");
+            //    //Console.WriteLine(Regex.Replace(randomWord[i], "([A-Z])", "_$1"));
+            //}
+
+
+
+            // Console.WriteLine("The string '{0}' contains '{1}'? -> '{0}'", randomWord, userInput, validUserInput, words);
+            // Console.WriteLine($"The string '{randomWord}' contains '{userInput}'? -> '{validUserInput}'");
+
         }
     }
+
 }
