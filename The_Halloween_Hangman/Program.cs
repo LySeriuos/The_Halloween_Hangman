cdc5bb9fde0605a5859c94de62c68ca15677fd0a
LySeriuos <drungilas.p@gmail.com>
2022-11-13 21:54:33 +0100

    got the solution for changing the underscores to letters

--- The_Halloween_Hangman/Program.cs
+++ The_Halloween_Hangman/Program.cs
@@ -11,65 +11,74 @@
                 "repetition", "unlawful", "arrogant", "jazz" };
             int wordsIndex = random.Next(words.Count); // picking random index in the list
             string randomWord = words[wordsIndex]; // getting the value of the index
-
             Console.WriteLine(randomWord); // printing random word in the Console 
-            char[] randomWordInChar = new char[randomWord.Length]; // creating char array for randomWord
-            randomWordInChar = randomWord.ToCharArray(); // converting to char array
             int randomWordsLength = randomWord.Length; // getting the length of the randomWord
             string[] underscores = new string[randomWordsLength]; // creating new array for underscores
-            var listOfUnderscoresString = new List<string>(); //creating new list for underscores
+            var listOfChars = new List<char>();
 
-            int i;
-            for (i = 0; i < underscores.Length; i++) // for every letter adding one underscore
+            int totFinds = 0;
+
+
+            for (int i = 0; i < underscores.Length; i++)
             {
-                listOfUnderscoresString.Add("_");
+                listOfChars.Add('_');
             }
 
-            string[] underscoreChar = listOfUnderscoresString.ToArray(); // converting list to array
-            for (i = 0; i < underscoreChar.Length; i++) // looping thru every undescore in the list to get indexnumber
+            do
             {
-                Console.Write(underscoreChar[i]);
-            }
+                //Console.WriteLine($"\n\n\r\n{listOfChars}");
+                char[] underscoreChars = listOfChars.ToArray();
+                for (int i = 0; i < underscoreChars.Length; i++)
+                {
+                    Console.Write(underscoreChars[i]);
+                }
 
-            char userInput; //creating char for the User Input
-            Console.WriteLine("\n\n\r\nGuess the letter!");
-            userInput = Convert.ToChar(Console.ReadLine());
-            
-           
+                Console.WriteLine("\n\n\r\nGuess the letter!"); // Asking for user's input
+                string userInput = Console.ReadLine(); // reading user's input
+                bool validUserInput = randomWord.Contains(userInput);
+                if (userInput != "") // check if user input is not empty 
+                {
+                    for (int i = 0; i < randomWord.Length; i++) // check thru the chosen word 
+                    {
 
+                        //        found = randomWord.IndexOf(userInput, i); //getting the indexes of the letters
+                        //         Console.Write(found);
+                        char currentChar = randomWord[i]; 
+                        char userChar = userInput[0];
 
+                        // Add for each to get thru every index with the same char
+                        if (userChar == currentChar)
+                        {            
+                            totFinds++; //counting finds
+                            listOfChars[i] = userInput[0];
+                            //                     listOfChars[found] = userInput; // updating the underscore by index to the letter by the same index                            
+                        }
 
+                        //if (found > 0) // if index is higher or equal to 0 we can count how many times the same letter was found
+                        //{
+                        //    i = found ; // getting index numbers
+                        //}
+                        //else
+                        //{
+                        //    break;
+                        //}
+                    }
+                }
+                else
+                {
+                    return;
+                }
 
+                Console.WriteLine($"The search parameter {userInput} was found {totFinds} times."); //TODO: use string interpolation.
 
+                totFinds = 0;
+                if (validUserInput != true) // if the users input doesn't match any letter it gives this error
+                {
+                    Console.WriteLine("Your letter doesn't match with any in the word");
+                }
+            } while (true);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            //for (int i = 0; i < randomWord.Count(); i++)
-            //{
-            //    Console.Write("the letters"[i]);
-            //    //Regex.Replace(randomWord, "([A-Z])", "_$1");
-            //    //Console.WriteLine(Regex.Replace(randomWord[i], "([A-Z])", "_$1"));
-            //}
-
-
-
-            // Console.WriteLine("The string '{0}' contains '{1}'? -> '{0}'", randomWord, userInput, validUserInput, words);
-            // Console.WriteLine($"The string '{randomWord}' contains '{userInput}'? -> '{validUserInput}'");
 
         }
     }
-
 }
