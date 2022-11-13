1d167e90ab6c5a194762d3d986dd4ead5c4d34eb
LySeriuos <drungilas.p@gmail.com>
2022-11-13 22:17:23 +0100

    cleaned code

--- The_Halloween_Hangman/Program.cs
+++ The_Halloween_Hangman/Program.cs
@@ -18,7 +18,6 @@
 
             int totFinds = 0;
 
-
             for (int i = 0; i < underscores.Length; i++)
             {
                 listOfChars.Add('_');
@@ -26,7 +25,6 @@
 
             do
             {
-                //Console.WriteLine($"\n\n\r\n{listOfChars}");
                 char[] underscoreChars = listOfChars.ToArray();
                 for (int i = 0; i < underscoreChars.Length; i++)
                 {
@@ -40,28 +38,14 @@
                 {
                     for (int i = 0; i < randomWord.Length; i++) // check thru the chosen word 
                     {
-
-                        //        found = randomWord.IndexOf(userInput, i); //getting the indexes of the letters
-                        //         Console.Write(found);
                         char currentChar = randomWord[i]; 
                         char userChar = userInput[0];
 
-                        // Add for each to get thru every index with the same char
-                        if (userChar == currentChar)
+                        if (userChar == currentChar) // checking if letter in the randomWord is the same as userInput
                         {            
                             totFinds++; //counting finds
-                            listOfChars[i] = userInput[0];
-                            //                     listOfChars[found] = userInput; // updating the underscore by index to the letter by the same index                            
+                            listOfChars[i] = userInput[0]; // changing underscore to letter in the listOfChars                                            
                         }
-
-                        //if (found > 0) // if index is higher or equal to 0 we can count how many times the same letter was found
-                        //{
-                        //    i = found ; // getting index numbers
-                        //}
-                        //else
-                        //{
-                        //    break;
-                        //}
                     }
                 }
                 else
@@ -69,16 +53,13 @@
                     return;
                 }
 
-                Console.WriteLine($"The search parameter {userInput} was found {totFinds} times."); //TODO: use string interpolation.
-
-                totFinds = 0;
+                Console.WriteLine($"The search parameter {userInput} was found {totFinds} times."); 
+                
                 if (validUserInput != true) // if the users input doesn't match any letter it gives this error
                 {
                     Console.WriteLine("Your letter doesn't match with any in the word");
                 }
             } while (true);
-
-
         }
     }
 }
