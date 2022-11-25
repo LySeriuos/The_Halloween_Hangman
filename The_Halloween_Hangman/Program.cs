7cefe02ecde3c938633fab8a05c8c5e30e60476d
LySeriuos <drungilas.p@gmail.com>
2022-11-25 23:39:37 +0100

    created bool variable to check if two lists are equals when order of both list is not important

--- The_Halloween_Hangman/Program.cs
+++ The_Halloween_Hangman/Program.cs
@@ -1,26 +1,30 @@
-﻿namespace The_Halloween_Hangman
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace The_Halloween_Hangman
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Random random = new Random(); // activating random function
-            var words = new List<string>() { "money", "danger", "aloof", "scheme", "observer", "abolish", "goalkeeper",
-                "conviction", "allowance", "arrogant", "separation", "environment",
-                "protection", "deteriorate", "economist", "continental", "mushroom",
-                "repetition", "unlawful", "arrogant", "jazz" };
+            var words = new List<string>() { "money", "danger", "aloof", "scheme", "jazz" };
             int wordsIndex = random.Next(words.Count); // picking random index in the list
             string randomWord = words[wordsIndex]; // getting the value of the index
-            char[] randomWordToCharArray = randomWord.ToCharArray();
+            
+            List<char> randomWordToCharList = new List<char>();
+            randomWordToCharList.AddRange(randomWord);
             Console.WriteLine(randomWord); // printing random word in the Console                
             int randomWordLength = randomWord.Length; // getting the length of the randomWord          
             var listOfChars = new List<char>(); // creating a chars list for underscores
             var listOfGoodLettersInChar = new List<char>();
             var listOfAllInputChars = new List<char>();
-            int tries = 0; 
+            int tries = 0;
             Console.WriteLine("\n\t\t\t\t\tWelcome to The Hangman! \n\n\t\tYou guess the word by guessing letters one at a time. \n\t\tYou are allowed seven mistakes! \n\n\t\t\t\t\tGood luck!!!");
             do
             {
+
                 Console.WriteLine();
                 for (int i = 0; i < randomWordLength; i++)
                 {
@@ -29,7 +33,7 @@
                 }
                 Console.WriteLine("\n\n\r\nGuess the letter!"); // Asking for user's input
                 string userInput = Console.ReadLine().ToLower(); // reading user's input
-                
+
 
                 if (userInput != "") // check if user input is not empty 
                 {
@@ -41,11 +45,12 @@
                     {
                         char currentChar = randomWord[i]; // converting string to char 
                         if (userChar == currentChar) // checking if letter in the randomWord is the same as userInput
-                        {   
+                        {
                             listOfChars[i] = userChar; // changing underscore to letter in the listOfChars
                             listOfGoodLettersInChar.Add(userChar); // adding to the list guessed right letters
                         }
                     }
+                    
                 }
                 else
                 {
@@ -57,9 +62,17 @@
                         listOfAllInputChars.Add(userCharAfterInputWasEmpty); // if it is not empty, add letter to the list of all letters
                     }
                 }
+                // checking if listOfGoodLettersInChar is equal to randomWordToCharList
+                bool isEqual = randomWordToCharList.All(listOfGoodLettersInChar.Contains) && listOfGoodLettersInChar.All(randomWordToCharList.Contains);
+                if (isEqual == true)
+                {
+                    Console.WriteLine("You won!"); // printing out message 
+                    break; // jumping out the code
+                }
 
                 var listOfBadLettersInChar = listOfAllInputChars.Except(listOfGoodLettersInChar).ToList(); // comparing two lists and creating new list
                                                                                                            // with letters wich doesn't match
+                
                 Console.WriteLine("Your's guessed wrong letters: "); // printing out wrong letters
                 foreach (char badLettersChar in listOfBadLettersInChar)
                 {
@@ -67,10 +80,10 @@
                 }
                 Console.WriteLine(); // empty line for extra empty row
 
+               
 
 
-            } while (tries < 7);
-            //TODO: Inform user of game result (win/lose)
+            } while (tries < 8);
         }
 
     }
